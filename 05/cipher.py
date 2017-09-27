1904e9f824ecef2c33848fa19c7b5474596b14aa
MylesAustin13 <myles.austin13@myhunter.cuny.edu>
2017-09-27 16:00:39 -0400

    Added homework file due 9-28-17

---
+++ 05/cipher.py
@@ -0,0 +1,41 @@
+caps_start = ord("A")
+lower_start = ord("a")
+caps_end = ord("Z")
+lower_end = ord("z")
+'''
+for letter in "abcdefg":
+    ascii_val = ord(letter)
+    plus = ascii_val + 1
+    newchar = chr(plus)
+    print(letter,":",ascii_val," rotated: ",newchar,":",plus)
+'''
+def encode_letter(l,r):
+    val = ord(l)
+    i = 0
+    while (i != r):
+        
+        if val == lower_end:
+            val = lower_start
+        elif val == caps_end:
+            val = caps_start
+        else:
+            val += 1
+        i += 1
+    return chr(val)
+
+def encode_string(s,r):
+    result = ""
+    for letter in s:
+        if (ord(letter) >= caps_start and ord(letter) <= caps_end) or (ord(letter) >= lower_start and ord(letter) <= lower_end):
+            result += encode_letter(letter,r)
+        else:
+            result += letter
+            
+    return result
+def full_encode(s):
+    for i in range(26):
+        print(encode_string(s,i) + "\n")
+
+print(encode_letter("z",5))
+print(encode_string("wxyz",4))
+full_encode("Hello, Player 1!")
